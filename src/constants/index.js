5657a9667649b7bc6d04445e66f1ed0841f55a07
Lucas Ferreira Silva <66436180+lucasfsilva2310@users.noreply.github.com>
2024-07-08 18:15:48 -0300

    removing api keys

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
-export const API_EDAMAN_KEY = "267283f1a3321a8d570fac0cf017d03e";
-export const API_EDAMAN_ID = "10531e7c";
-export const API_YOUTUBE_KEY = "AIzaSyCSOOzfIafqx5KVilmPA5yKF6vi1GroRvI";
+
+
+export const API_EDAMAN_KEY = "";
+export const API_EDAMAN_ID = "";
+export const API_YOUTUBE_KEY = "";
